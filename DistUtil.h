bbbc50bc3b6847515122269ebd9d23a58f20c2b8
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-09-29 18:42:28 +0200

    pass arg by const ref

--- DistUtil.h
+++ DistUtil.h
@@ -9,7 +9,7 @@ namespace sc2util {
 	// sorting using a distance matrix, with respect to one element ti
 	std::vector<int> sortByDistanceTo(std::valarray<float> matrix, int ti, size_t sz);
 	// computing a distance matrix, with actual pathing queries
-	std::valarray<float> computeDistanceMatrix(const std::vector<sc2::Point3D> expansions, sc2::QueryInterface * query);
+	std::valarray<float> computeDistanceMatrix(const std::vector<sc2::Point3D> & expansions, sc2::QueryInterface * query);
 	// utility for center of gravity
 	const sc2::Point2D cog(const std::vector<sc2::Point2D>& points);
 }
