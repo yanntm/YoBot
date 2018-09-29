f2caa0f9429b38664c7729acf75e4e450f0a6209
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-09-29 17:18:49 +0200

    Yo Agent now has a Map Topology

--- YoAgent.h
+++ YoAgent.h
@@ -6,6 +6,8 @@
 #include "sc2api/sc2_map_info.h"
 #include "sc2lib/sc2_lib.h"
 
+#include "MapTopology.h"
+
 #define DllExport   __declspec( dllexport )  
 
 using namespace sc2;
@@ -13,9 +15,11 @@ using namespace sc2;
 // A Yo Agent, is an agent that already has some nice stuff it knows about world and game state
 // that is not generally available in API
 class YoAgent : public Agent {
-
-
-
-
+public : 
+	void initialize() {
+		map.init(Observation(), Query(), Debug());
+	}
+protected :
+	MapTopology map;
 };
 
