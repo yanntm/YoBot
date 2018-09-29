4c930943e48d81e49bb399bbbbc439e9b3e68148
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-09-29 13:27:04 +0200

    refactor : extracting code to parts

---
+++ UnitTypes.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "sc2api/sc2_interfaces.h"
+
+namespace sc2util {
+	// useful functions for filtering and classifying units
+	
+	// all mineral types
+	bool IsMineral(sc2::UNIT_TYPEID type);
+	// all geyser types
+	bool IsVespene(sc2::UNIT_TYPEID type);
+	// workers + mules
+	bool IsWorkerType(sc2::UNIT_TYPEID type);
+	// if it produces workers, it's a centre
+	bool IsCommandStructure(sc2::UNIT_TYPEID type);
+	// if it's a building that attacks you
+	bool isStaticDefense(sc2::UNIT_TYPEID type);
+	// a moving enemy that hits from range (TODO : list very incomplete)
+	bool IsRangedUnit(sc2::UNIT_TYPEID type);
+	// any building
+	bool IsBuilding(sc2::UNIT_TYPEID type);
+	// any moving enemy with a weapon
+	bool IsArmyUnitType(sc2::UNIT_TYPEID type);
+}
--- YoBot.h
+++ YoBot.h
@@ -4,11 +4,14 @@
 #include "sc2api/sc2_map_info.h"
 #include "sc2lib/sc2_lib.h"
 
+#include "UnitTypes.h"
+
 #include <valarray>
 
 //#include "Strategys.h"
 #define DllExport   __declspec( dllexport )  
 using namespace sc2;
+using namespace sc2util;
 
 class YoBot : public Agent {
 public:
@@ -1920,175 +1923,10 @@ private:
 		return pot[ti];
 	}
 
-	static bool IsMineral(const sc2::UNIT_TYPEID type) {
-		switch (type) {
-			case  UNIT_TYPEID::NEUTRAL_MINERALFIELD:case  UNIT_TYPEID::NEUTRAL_MINERALFIELD750:case  UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD:
-			case  UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD750:case  UNIT_TYPEID::NEUTRAL_PURIFIERMINERALFIELD:case  UNIT_TYPEID::NEUTRAL_PURIFIERMINERALFIELD750:
-			case  UNIT_TYPEID::NEUTRAL_PURIFIERRICHMINERALFIELD:case  UNIT_TYPEID::NEUTRAL_PURIFIERRICHMINERALFIELD750:case  UNIT_TYPEID::NEUTRAL_LABMINERALFIELD:
-			case  UNIT_TYPEID::NEUTRAL_LABMINERALFIELD750:case  UNIT_TYPEID::NEUTRAL_BATTLESTATIONMINERALFIELD:case  UNIT_TYPEID::NEUTRAL_BATTLESTATIONMINERALFIELD750:
-			return true;
-		}
-		return false;
-	}
-
-	static bool IsVespene(const sc2::UNIT_TYPEID type) {
-		switch (type) {
-		case  UNIT_TYPEID::NEUTRAL_VESPENEGEYSER:case  UNIT_TYPEID::NEUTRAL_PROTOSSVESPENEGEYSER:case  UNIT_TYPEID::NEUTRAL_SHAKURASVESPENEGEYSER:
-		case  UNIT_TYPEID::NEUTRAL_RICHVESPENEGEYSER:case  UNIT_TYPEID::NEUTRAL_PURIFIERVESPENEGEYSER:
-			return true;
-		}
-		return false;
-	}
-
-	static bool IsWorkerType(const sc2::UNIT_TYPEID type) 
-	{
-		switch (type)
-		{
-		case sc2::UNIT_TYPEID::TERRAN_SCV:
-		case sc2::UNIT_TYPEID::TERRAN_MULE: 
-		case sc2::UNIT_TYPEID::PROTOSS_PROBE:
-		case sc2::UNIT_TYPEID::ZERG_DRONE: 
-		case sc2::UNIT_TYPEID::ZERG_DRONEBURROWED: return true;
-		default: return false;
-		}
-	}
-
-	static bool IsCommandStructure(const sc2::UNIT_TYPEID type) {
-		switch (type) {
-		case UNIT_TYPEID::TERRAN_COMMANDCENTER:
-		case UNIT_TYPEID::TERRAN_COMMANDCENTERFLYING:
-		case UNIT_TYPEID::TERRAN_ORBITALCOMMAND:		
-		case UNIT_TYPEID::TERRAN_PLANETARYFORTRESS:
-		case UNIT_TYPEID::TERRAN_ORBITALCOMMANDFLYING:
-		case UNIT_TYPEID::PROTOSS_NEXUS:
-		case UNIT_TYPEID::ZERG_HATCHERY:
-		case UNIT_TYPEID::ZERG_LAIR:
-		case UNIT_TYPEID::ZERG_HIVE: return true;
-		default: return false;
-		}
-	}
-
-	static bool isStaticDefense(const sc2::UNIT_TYPEID type) {
-		switch (type)
-		{
-		case sc2::UNIT_TYPEID::ZERG_SPINECRAWLER:
-		case sc2::UNIT_TYPEID::ZERG_SPINECRAWLERUPROOTED:
-		case sc2::UNIT_TYPEID::PROTOSS_PHOTONCANNON:
-		case sc2::UNIT_TYPEID::PROTOSS_SHIELDBATTERY:
-		case sc2::UNIT_TYPEID::TERRAN_BUNKER:
-		case sc2::UNIT_TYPEID::TERRAN_PLANETARYFORTRESS:
-			return true;
-		}
-		return false;
-	}
-
-	static bool IsRangedUnit(const sc2::UNIT_TYPEID type)
-	{
-		switch (type)
-		{
-		case UNIT_TYPEID::TERRAN_MARINE:
-		case UNIT_TYPEID::TERRAN_MARAUDER:
-		case UNIT_TYPEID::PROTOSS_ADEPT:
-		case UNIT_TYPEID::PROTOSS_STALKER:
-		case UNIT_TYPEID::ZERG_ROACH:
-			return true;
-		}
-		return false;
-	}
-
-	static bool IsBuilding(const sc2::UNIT_TYPEID type) 
-	{
-		if (IsCommandStructure(type)) {
-			return true;
-		}
-		switch (type)
-		{
-			//Terran
-		case sc2::UNIT_TYPEID::TERRAN_ARMORY:
-		case sc2::UNIT_TYPEID::TERRAN_BARRACKS:
-		case sc2::UNIT_TYPEID::TERRAN_BARRACKSFLYING:
-		case sc2::UNIT_TYPEID::TERRAN_BARRACKSREACTOR:
-		case sc2::UNIT_TYPEID::TERRAN_BUNKER:
-		case sc2::UNIT_TYPEID::TERRAN_BARRACKSTECHLAB:
-		case sc2::UNIT_TYPEID::TERRAN_COMMANDCENTER:		
-		case sc2::UNIT_TYPEID::TERRAN_ENGINEERINGBAY:
-		case sc2::UNIT_TYPEID::TERRAN_FACTORY:
-		case sc2::UNIT_TYPEID::TERRAN_FACTORYFLYING:
-		case sc2::UNIT_TYPEID::TERRAN_FACTORYREACTOR:
-		case sc2::UNIT_TYPEID::TERRAN_FACTORYTECHLAB:
-		case sc2::UNIT_TYPEID::TERRAN_FUSIONCORE:
-		case sc2::UNIT_TYPEID::TERRAN_GHOSTACADEMY:
-		case sc2::UNIT_TYPEID::TERRAN_MISSILETURRET:		
-		case sc2::UNIT_TYPEID::TERRAN_PLANETARYFORTRESS:
-		case sc2::UNIT_TYPEID::TERRAN_REFINERY:
-		case sc2::UNIT_TYPEID::TERRAN_SENSORTOWER:
-		case sc2::UNIT_TYPEID::TERRAN_STARPORT:
-		case sc2::UNIT_TYPEID::TERRAN_STARPORTFLYING:
-		case sc2::UNIT_TYPEID::TERRAN_STARPORTREACTOR:
-		case sc2::UNIT_TYPEID::TERRAN_STARPORTTECHLAB:
-		case sc2::UNIT_TYPEID::TERRAN_SUPPLYDEPOT:
-		case sc2::UNIT_TYPEID::TERRAN_SUPPLYDEPOTLOWERED:
-		case sc2::UNIT_TYPEID::TERRAN_REACTOR:
-		case sc2::UNIT_TYPEID::TERRAN_TECHLAB:
-
-			// Zerg
-		case sc2::UNIT_TYPEID::ZERG_BANELINGNEST:
-		case sc2::UNIT_TYPEID::ZERG_CREEPTUMOR:
-		case sc2::UNIT_TYPEID::ZERG_CREEPTUMORBURROWED:
-		case sc2::UNIT_TYPEID::ZERG_CREEPTUMORQUEEN:
-		case sc2::UNIT_TYPEID::ZERG_EVOLUTIONCHAMBER:
-		case sc2::UNIT_TYPEID::ZERG_EXTRACTOR:
-		case sc2::UNIT_TYPEID::ZERG_GREATERSPIRE:
-		case sc2::UNIT_TYPEID::ZERG_HYDRALISKDEN:
-		case sc2::UNIT_TYPEID::ZERG_INFESTATIONPIT:
-		case sc2::UNIT_TYPEID::ZERG_LURKERDENMP:
-		case sc2::UNIT_TYPEID::ZERG_NYDUSCANAL:
-		case sc2::UNIT_TYPEID::ZERG_NYDUSNETWORK:
-		case sc2::UNIT_TYPEID::ZERG_SPINECRAWLER:
-		case sc2::UNIT_TYPEID::ZERG_SPINECRAWLERUPROOTED:
-		case sc2::UNIT_TYPEID::ZERG_SPAWNINGPOOL:
-		case sc2::UNIT_TYPEID::ZERG_SPIRE:
-		case sc2::UNIT_TYPEID::ZERG_SPORECRAWLER:
-		case sc2::UNIT_TYPEID::ZERG_SPORECRAWLERUPROOTED:
-		case sc2::UNIT_TYPEID::ZERG_ULTRALISKCAVERN:
-
-			// Protoss
-		case sc2::UNIT_TYPEID::PROTOSS_ASSIMILATOR:
-		case sc2::UNIT_TYPEID::PROTOSS_CYBERNETICSCORE:
-		case sc2::UNIT_TYPEID::PROTOSS_DARKSHRINE:
-		case sc2::UNIT_TYPEID::PROTOSS_FLEETBEACON:
-		case sc2::UNIT_TYPEID::PROTOSS_FORGE:
-		case sc2::UNIT_TYPEID::PROTOSS_PHOTONCANNON:
-		case sc2::UNIT_TYPEID::PROTOSS_SHIELDBATTERY:
-		case sc2::UNIT_TYPEID::PROTOSS_GATEWAY:
-		case sc2::UNIT_TYPEID::PROTOSS_PYLON:
-		case sc2::UNIT_TYPEID::PROTOSS_PYLONOVERCHARGED:
-		case sc2::UNIT_TYPEID::PROTOSS_ROBOTICSBAY:
-		case sc2::UNIT_TYPEID::PROTOSS_ROBOTICSFACILITY:
-		case sc2::UNIT_TYPEID::PROTOSS_STARGATE:
-		case sc2::UNIT_TYPEID::PROTOSS_TEMPLARARCHIVE:
-		case sc2::UNIT_TYPEID::PROTOSS_TWILIGHTCOUNCIL:
-		case sc2::UNIT_TYPEID::PROTOSS_WARPGATE:
-			return true;
-		}
-		return false;
-	}
-
-	static bool IsArmyUnitType(const sc2::UNIT_TYPEID type) 
-	{
-		if (IsWorkerType(type)) { return false; }
-		if (type == sc2::UNIT_TYPEID::ZERG_OVERLORD) { return false; } //Excluded here the overlord transport etc to count them as army unit
-		if (IsBuilding(type)) { return false; }
-		if (type == sc2::UNIT_TYPEID::ZERG_EGG) { return false; }
-		if (type == sc2::UNIT_TYPEID::ZERG_LARVA) { return false; }
-
-		return true;
-	}
-
-	const Unit* FindNearestMineralPatch(const Point2D& start) {		
+	const Unit* FindNearestMineralPatch(const Point2D& start) {
 		auto m = FindNearestUnit(start, Observation()->GetUnits(Unit::Alliance::Neutral, [](const auto & u) { return IsMineral(u.unit_type); }));
-		if (m == nullptr) {				
-			m = FindNearestUnit(start, Observation()->GetUnits(Unit::Alliance::Neutral, [](const auto & u) { return IsMineral(u.unit_type); }),10000.0);
+		if (m == nullptr) {
+			m = FindNearestUnit(start, Observation()->GetUnits(Unit::Alliance::Neutral, [](const auto & u) { return IsMineral(u.unit_type); }), 10000.0);
 		}
 		return m;
 	}
