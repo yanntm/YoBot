d9199cb51bb6918b57401c4006ad91c0b2cb6a5a
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-15 18:50:49 +0100

    heavy refactoring added power (pylons)

--- BOBuilder.cpp
+++ BOBuilder.cpp
@@ -1,13 +1,12 @@
 #include "BOBuilder.h"
+#include "UnitTypes.h"
 #include <unordered_set>
 
 namespace suboo {
 
-	BuildOrder BOBuilder::computeBO()
-	{
-		BuildOrder bo;
+	BuildOrder BOBuilder::makeBOFromGoal() {
 		auto & tech = TechTree::getTechTree();
-
+		BuildOrder bo;
 		// make the basic thing : list of creation order of each desired units x qty
 		for (auto & goal : goals) {
 			for (int index = 0; index < tech.size(); index++) {
@@ -18,7 +17,11 @@ namespace suboo {
 				}
 			}
 		}
+		return bo;
+	}
 
+	static BuildOrder enforcePrereq(const BuildOrder & bo) {
+		auto & tech = TechTree::getTechTree();
 		// enforce prerequisites
 		GameState state = tech.getInitial();
 		BuildOrder bopre;
@@ -27,9 +30,9 @@ namespace suboo {
 			if (bi.getAction() == BUILD) {
 				auto target = bi.getTarget();
 				auto & unit = tech.getUnitById(target);
-				
+
 				// unit prereq
-				for (UnitId prereq = unit.prereq; (int)prereq != 0 ; prereq = tech.getUnitById(prereq).prereq ) {
+				for (UnitId prereq = unit.prereq; (int)prereq != 0; prereq = tech.getUnitById(prereq).prereq) {
 					if (!state.hasUnit(prereq)) {
 						bopre.addItemFront(prereq);
 						state.addUnit(prereq);
@@ -45,15 +48,50 @@ namespace suboo {
 						bopre.addItemFront(prereq);
 						state.addUnit(prereq);
 						seen.insert(prereq);
-					}					
+					}
 				}
-				
+
 
 				bopre.addItem(target);
 				state.addUnit(target);
 			}
 		}
 		return bopre;
+	}
+
+	static BuildOrder addPower(const BuildOrder & bo) {
+		auto & tech = TechTree::getTechTree();
+		BuildOrder bopre;
+		auto state = tech.getInitial();
+		for (auto & bi : bo.getItems()) {
+			if (bi.getAction() == BUILD) {
+				auto target = bi.getTarget();
+				auto & unit = tech.getUnitById(target);
+				// deal with power				
+				if (target != UnitId::PROTOSS_NEXUS
+					&& target != UnitId::PROTOSS_PYLON
+					&& unit.builder == UnitId::PROTOSS_PROBE
+					&& sc2util::IsBuilding(target)
+					)
+				{
+					auto prereq = UnitId::PROTOSS_PYLON;
+					if (!state.hasUnit(prereq)) {
+						bopre.addItemFront(prereq);
+						state.addUnit(prereq);
+					}
+				}
+				bopre.addItem(target);
+			}
+		}
+		return bopre;
+	}
+
+	BuildOrder BOBuilder::computeBO()
+	{
+		BuildOrder bo = makeBOFromGoal();
+		bo = enforcePrereq(bo);
+		bo = addPower(bo);
+		return bo;
 	}
 
 }
