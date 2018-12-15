197e3afca4acae68e632ac5a6b9e1e3fe17d1492
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-15 17:43:51 +0100

    add prerequisites for units to a buildorder

--- BOBuilder.cpp
+++ BOBuilder.cpp
@@ -19,7 +19,34 @@ namespace suboo {
 			}
 		}
 
-		return bo;
+		// enforce prerequisites
+		GameState state = tech.getInitial();
+		BuildOrder bopre;
+		for (auto & bi : bo.getItems()) {
+			if (bi.getAction() == BUILD) {
+				auto target = bi.getTarget();
+				auto & unit = tech.getUnitById(target);
+				// unit prereq
+				for (UnitId prereq = unit.prereq; (int)prereq != 0 ; prereq = tech.getUnitById(prereq).prereq ) {
+					if (!state.hasUnit(prereq)) {
+						bopre.addItemFront(prereq);
+						state.addUnit(prereq);
+					}
+				}
+				// builder and prereq
+				for (UnitId prereq = unit.builder; (int)prereq != 0; prereq = tech.getUnitById(prereq).builder) {
+					if (!state.hasUnit(prereq)) {
+						bopre.addItemFront(prereq);
+						state.addUnit(prereq);
+					}
+				}
+				
+
+				bopre.addItem(target);
+				state.addUnit(target);
+			}
+		}
+		return bopre;
 	}
 
 }
