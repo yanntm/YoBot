98892cd236a5f73fca7732ec6e9106f5e93dbb59
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-17 00:07:28 +0100

    distinguish in hasUnit : free or busy ? workers harvest minerals by default typo best = -1 in test nicer state printout gas workers go by packs of 3

--- BuildOrder.cpp
+++ BuildOrder.cpp
@@ -73,11 +73,18 @@ namespace suboo {
 			out << "'" << time;
 		}
 	}
-	bool GameState::hasUnit(UnitId unit) const
+	bool GameState::hasFreeUnit(UnitId unit) const
 	{
 		return std::any_of(units.begin(), units.end(), [unit](auto & u) {
 			return (u.type == unit && u.state == u.FREE) 
 				|| (sc2util::IsWorkerType(unit) && u.state == u.MINING_MINERALS); 
+		});
+	}
+	bool GameState::hasFinishedUnit(UnitId unit) const
+	{
+		return std::any_of(units.begin(), units.end(), [unit](auto & u) {
+			return (u.type == unit && u.state != u.BUILDING)
+				;
 		});
 	}
 	void GameState::addUnit(UnitId unit)
@@ -166,7 +173,13 @@ namespace suboo {
 				if (u.time_to_free > 0) {
 					u.time_to_free--;
 					if (u.time_to_free == 0) {
-						u.state = u.FREE;
+						if (!sc2util::IsWorkerType(u.type)) {
+							u.state = u.FREE;
+						}
+						else {
+							// default for workers is to mine
+							u.state = u.MINING_MINERALS;
+						}
 						changed = true;
 					}
 				}
@@ -229,7 +242,7 @@ namespace suboo {
 		int best = -1;
 		for (auto & u : units) {
 			if (u.type == id && u.state != u.FREE && u.time_to_free != 0) {
-				if (best = -1 || units[best].time_to_free > u.time_to_free) {
+				if (best == -1 || units[best].time_to_free > u.time_to_free) {
 					best = index;
 				}
 			}
@@ -239,34 +252,55 @@ namespace suboo {
 			return false;
 		}
 		else {
-			std::cout << "Waited for " << TechTree::getTechTree().getUnitById(id).name << " to be free for " << units[best].time_to_free << "s." << std::endl;
+			// std::cout << "Waited for " << TechTree::getTechTree().getUnitById(id).name << " to be free for " << units[best].time_to_free << "s." << std::endl;
 			stepForward(units[best].time_to_free);
 			return true;
 		}
 	}
-	bool GameState::assignProbe(UnitInstance::UnitState state)
+	bool GameState::waitforFreeSupply(int needed)
 	{
-		bool done = false;
-		for (auto & u : units) {
-			if (sc2util::IsWorkerType(u.type) && u.state == u.FREE) {
-				u.state = UnitInstance::MINING_VESPENE;
-				vps = -1;
-				done = true;
-				break;
-			}
+		int cur = getAvailableSupply();
+		if (cur >= needed) {
+			return true;
 		}
-		if (!done) {
-			for (auto & u : units) {
-				if (sc2util::IsWorkerType(u.type) && u.state == u.MINING_MINERALS) {
-					u.state = UnitInstance::MINING_VESPENE;
-					vps = -1;
-					mps = -1;
-					done = true;
-					break;
+		auto it = std::find_if(units.begin(), units.end(), [](auto & u) {return u.type == UnitId::PROTOSS_PYLON && u.state != u.FREE ; });
+		if (it == units.end()) {
+			return false;
+		}
+		int index = 0;
+		int best = -1;
+		for (auto & u : units) {
+			if (u.state == u.BUILDING && (u.type == UnitId::PROTOSS_PYLON || u.type == UnitId::PROTOSS_NEXUS)) {
+				if (best == -1 || units[best].time_to_free > u.time_to_free) {
+					best = index;
 				}
 			}
+			index++;
 		}
-		return done;
+		if (best == -1) {
+			return false;
+		}
+		else {
+			// std::cout << "Waited for " << TechTree::getTechTree().getUnitById(id).name << " to be provide food for " << units[best].time_to_free << "s." << std::endl;
+			stepForward(units[best].time_to_free);
+			return true;
+		}
+		
+	}
+	bool GameState::assignProbe(UnitInstance::UnitState state)
+	{
+		int done = 0;
+		for (auto & u : units) {
+			if (sc2util::IsWorkerType(u.type) && ( u.state == u.FREE || u.state == u.MINING_MINERALS) ) {
+				u.state = UnitInstance::MINING_VESPENE;
+				vps = -1;
+				mps = -1;
+				done++;
+				if (done >= 3) 
+					break;
+			}
+		}
+		return done>=3;
 	}
 	bool GameState::assignFreeUnit(UnitId type, UnitInstance::UnitState state, int time)
 	{
@@ -282,10 +316,32 @@ namespace suboo {
 	void GameState::print(std::ostream & out) const
 	{
 		auto & tech = TechTree::getTechTree();
+		std::unordered_map<UnitId, std::unordered_map<UnitInstance::UnitState, int> > perUnitPerState;
 		for (auto & u : units) {
-			u.print(out);
+			auto it = perUnitPerState.find(u.type);
+			if (it == perUnitPerState.end()) {
+				perUnitPerState[u.type] = { {u.state,1} };
+			}
+			else {
+				auto & perState = it->second;
+				auto jt = perState.find(u.state);
+				if (jt == perState.end()) {
+					perState[u.state] = 1;
+				}
+				else
+				{
+					jt->second++;
+				}
+			}
+		}
+		for (auto it : perUnitPerState) {
+			out << tech.getUnitById(it.first).name << " ";
+			for (auto jt : it.second) {
+				out << jt.second << " x " << to_string(jt.first) << " ";
+			}
 			out << ",";
 		}
+
 		out << std::endl;
 		out << "bank : minerals = " << minerals << "("<< getMineralsPerSecond() <<"/s)" << " vespene = " << vespene << "(" << getVespenePerSecond() << "/s)"<< " supply : " << getAvailableSupply() << ":" << getUsedSupply() << "/" << getMaxSupply() <<  std::endl;
 	}
@@ -300,28 +356,34 @@ namespace suboo {
 	void UnitInstance::print(std::ostream & out) const
 	{
 		out << TechTree::getTechTree().getUnitById(type).name;
-		std::string status;
-		switch (state) {
-		case BUILDING :
-			status = "B";
-			break;
-		case MINING_MINERALS :
-			status = "M";
-			break;
-		case MINING_VESPENE :
-			status = "V";
-			break;
-		case BUSY :
-			status = "C";
-			break;
-		case FREE:
-		default:
-			status = "F";
-			break;
-		}
+		std::string status = to_string(state);
+		
 		out << "(" << status;
 		if (time_to_free > 0)
 			out << time_to_free;
 		out << ")";
+	}
+	std::string to_string(const UnitInstance::UnitState &state)
+	{
+		std::string status;
+		switch (state) {
+		case UnitInstance::BUILDING:
+			status = "B";
+			break;
+		case UnitInstance::MINING_MINERALS:
+			status = "M";
+			break;
+		case UnitInstance::MINING_VESPENE:
+			status = "V";
+			break;
+		case UnitInstance::BUSY:
+			status = "C";
+			break;
+		case UnitInstance::FREE:
+		default:
+			status = "F";
+			break;
+		}
+		return status;
 	}
 }
