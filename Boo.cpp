37ca30270908760f612e05376b0afc3f421ab618
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-16 17:40:34 +0100

    starting to add some mutators that make general good sense

---
+++ Boo.cpp
@@ -0,0 +1,121 @@
+#include "Boo.h"
+#include "BOBuilder.h"
+#include <iostream>
+#include <sstream>
+
+namespace suboo {
+
+	std::pair<int, BuildOrder> LeftShifter::improve(const BuildOrder & base)
+	{
+		auto & tech = TechTree::getTechTree();
+		std::vector<BuildOrder> candidates;
+		std::vector<std::string > candindexes;
+		candidates.reserve(20);
+		// find any adjacent events with same timing
+		auto & items = base.getItems();
+		for (int i = 0, e = items.size(); i < e; i++) {
+			auto & bi = items[i];
+			int skip = 0;
+			for (int j = i + 1; j < e; j++) {
+				auto & bj = items[j];
+				if (! (bj == bi)) {
+					bool biprecedesbj = true;
+					if (bi.getAction() == BUILD && bj.getAction() == BUILD) {
+						auto & ui = tech.getUnitById(bi.getTarget());
+						auto & uj = tech.getUnitById(bj.getTarget());
+						if (uj.prereq != ui.type && (uj.builder != ui.type || uj.builder == UnitId::PROTOSS_PROBE )) {
+							biprecedesbj = false;
+						}
+					}
+					if (bj.getAction() == TRANSFER_VESPENE && bi.getAction() == BUILD && bi.getTarget() == UnitId::PROTOSS_ASSIMILATOR) {
+
+					}
+					else {
+						if (bi.getAction() == BUILD && bj.getAction() != BUILD) {
+							biprecedesbj = false;
+						}
+					}
+					if (bj.getTime() == bi.getTime() || !biprecedesbj) {
+						BuildOrder bo = base;
+						std::stringstream sstr;
+						sstr << "swapping (" << i << " :"; bo.getItems()[i].print(sstr);
+						sstr << ") and (" << j << " :"; bo.getItems()[j].print(sstr);
+						sstr << ")" ;
+						bo.swapItems(i, j);
+
+						candidates.emplace_back(bo);
+						candindexes.emplace_back(sstr.str());
+					}
+					break;					
+				}
+				else {
+					skip++;
+				}
+			}
+			i += skip;
+		}
+		int best = base.getFinal().getTimeStamp();
+		int bestindex = -1;
+		int index = 0;
+		for (auto & bo : candidates) {
+			if (timeBO(bo)) {
+				auto total = bo.getFinal().getTimeStamp();
+				std::cout << candindexes[index] << " feasible in " << total ;
+				if (total < best) {
+					bestindex = index;
+					best = bo.getFinal().getTimeStamp();
+					std::cout << " (new best)" ;
+				}
+				std::cout << std::endl;
+			}
+			else {
+				std::cout << " unfeasible." << std::endl;
+			}
+			index++;
+		}
+		
+		return { base.getFinal().getTimeStamp() - best, bestindex == -1 ? BuildOrder() : candidates[bestindex]};
+	}
+
+
+
+	std::pair<int, BuildOrder> AddVespeneGatherer::improve(const BuildOrder & base)
+	{
+		int nexi = base.getFinal().countUnit(UnitId::PROTOSS_NEXUS);
+		int ass = base.getFinal().countUnit(UnitId::PROTOSS_ASSIMILATOR);
+		if (base.getFinal().getVespene() < 20 && 2 *nexi > ass) {
+			BuildOrder candidate = base;
+			for (int i = 0; i < 3; i++) {
+				candidate.addItemFront(TRANSFER_VESPENE);
+			}
+			candidate.addItemFront(UnitId::PROTOSS_ASSIMILATOR);
+			if (timeBO(candidate)) {
+				int delta =  base.getFinal().getTimeStamp() - candidate.getFinal().getTimeStamp() ;
+				if (delta > 0) {
+					return {delta, candidate };
+				}
+			}
+		}
+		return std::pair<int, BuildOrder>(0,BuildOrder());
+		
+	}
+
+	std::pair<int, BuildOrder> AddMineralGatherer::improve(const BuildOrder & base)
+	{
+		int nexi = base.getFinal().countUnit(UnitId::PROTOSS_NEXUS);
+		int ass = base.getFinal().countUnit(UnitId::PROTOSS_ASSIMILATOR);
+		int prob = base.getFinal().countUnit(UnitId::PROTOSS_PROBE);
+		if (base.getFinal().getMinerals() < 20 && (20 * nexi + 3*ass) > prob) {
+			BuildOrder candidate = base;
+			candidate.addItemFront(UnitId::PROTOSS_PROBE);
+			if (timeBO(candidate)) {
+				int delta = base.getFinal().getTimeStamp() - candidate.getFinal().getTimeStamp();
+				if (delta > 0) {
+					return { delta, candidate };
+				}
+			}
+		}
+		return std::pair<int, BuildOrder>(0, BuildOrder());		return std::pair<int, BuildOrder>();
+	}
+
+}
---
+++ Boo.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "BuildOrder.h"
+
+namespace suboo {
+
+	// a BO Optimizer interface
+	class boo {
+	public :
+		// returns delta seconds gained, 0 means unable to optimize
+		// if delta > 0, an improved BuildOrder is returned 
+		virtual std::pair<int,BuildOrder> improve(const BuildOrder & base)=0;
+		virtual const char* getName() const = 0;
+		virtual ~boo() {}
+	};
+	using pboo = std::unique_ptr<boo> ;
+
+	// a basic "improver" terminal/leaf
+	// when meeting adjacent builditems bi0, bi1, suppose bi0 was responsible 
+	// for delaying bi1. Shift bi1 left (permute the two events if not prereq contradiction) and measure improvement.
+	// macro blocks are built for adjacent equal actions.
+	class LeftShifter : public boo {
+		std::pair<int, BuildOrder> improve(const BuildOrder & base) ;
+		const char* getName() const { return "LeftShifter";  }
+	};
+
+	// if a build is gas starved, and the number of bases permits it add a assimilator
+	class AddVespeneGatherer : public boo {
+		std::pair<int, BuildOrder> improve(const BuildOrder & base);
+		const char* getName() const { return "AddVespeneGatherer"; }
+	};
+
+	// if a build is mineral starved, and the number of bases permits it add a worker
+	class AddMineralGatherer : public boo {
+		std::pair<int, BuildOrder> improve(const BuildOrder & base);
+		const char* getName() const { return "AddMineralGatherer"; }
+	};
+
+
+
+}
