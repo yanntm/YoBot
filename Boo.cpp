93830072bf392a4def4f115e3e20c12b9ac1f9c0
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-17 00:05:24 +0100

    refactor a function to select from candidates lots of changes in Leftshifter for more matches add probe is smarter about pylon saturated builds add production stutters on prod buildings

--- Boo.cpp
+++ Boo.cpp
@@ -1,9 +1,34 @@
 #include "Boo.h"
 #include "BOBuilder.h"
+#include "UnitTypes.h"
 #include <iostream>
 #include <sstream>
 
 namespace suboo {
+
+	std::pair<int, BuildOrder> findBest(const BuildOrder & base, std::vector<BuildOrder> & candidates, std::vector<std::string> candDesc) {
+		int best = base.getFinal().getTimeStamp();
+		int bestindex = -1;
+		int index = 0;
+		for (auto & bo : candidates) {
+			if (timeBO(bo)) {
+				auto total = bo.getFinal().getTimeStamp();
+				std::cout << candDesc[index] << " feasible in " << total;
+				if (total < best) {
+					bestindex = index;
+					best = bo.getFinal().getTimeStamp();
+					std::cout << " (new best)";
+				}
+				std::cout << std::endl;
+			}
+			else {
+				std::cout << candDesc[index] << " unfeasible." << std::endl;
+			}
+			index++;
+		}
+
+		return { base.getFinal().getTimeStamp() - best, bestindex == -1 ? BuildOrder() : candidates[bestindex] };
+	}
 
 	std::pair<int, BuildOrder> LeftShifter::improve(const BuildOrder & base)
 	{
@@ -13,6 +38,7 @@ namespace suboo {
 		candidates.reserve(20);
 		// find any adjacent events with same timing
 		auto & items = base.getItems();
+		GameState current = tech.getInitial();
 		for (int i = 0, e = items.size(); i < e; i++) {
 			auto & bi = items[i];
 			int skip = 0;
@@ -23,7 +49,7 @@ namespace suboo {
 					if (bi.getAction() == BUILD && bj.getAction() == BUILD) {
 						auto & ui = tech.getUnitById(bi.getTarget());
 						auto & uj = tech.getUnitById(bj.getTarget());
-						if (uj.prereq != ui.type && (uj.builder != ui.type || uj.builder == UnitId::PROTOSS_PROBE )) {
+						if (uj.prereq != ui.type && (uj.builder != ui.type || uj.builder == UnitId::PROTOSS_PROBE || current.hasFinishedUnit(uj.builder) )) {
 							biprecedesbj = false;
 						}
 					}
@@ -46,35 +72,20 @@ namespace suboo {
 						candidates.emplace_back(bo);
 						candindexes.emplace_back(sstr.str());
 					}
-					break;					
+					if (!(j < e - 1 && items[j + 1] == items[j])) {
+						break;
+					}
 				}
 				else {
-					skip++;
+					//skip++;
 				}
+			}
+			if (bi.getAction() == BUILD) {
+				current.addUnit(bi.getTarget());
 			}
 			i += skip;
 		}
-		int best = base.getFinal().getTimeStamp();
-		int bestindex = -1;
-		int index = 0;
-		for (auto & bo : candidates) {
-			if (timeBO(bo)) {
-				auto total = bo.getFinal().getTimeStamp();
-				std::cout << candindexes[index] << " feasible in " << total ;
-				if (total < best) {
-					bestindex = index;
-					best = bo.getFinal().getTimeStamp();
-					std::cout << " (new best)" ;
-				}
-				std::cout << std::endl;
-			}
-			else {
-				std::cout << " unfeasible." << std::endl;
-			}
-			index++;
-		}
-		
-		return { base.getFinal().getTimeStamp() - best, bestindex == -1 ? BuildOrder() : candidates[bestindex]};
+		return findBest(base, candidates,candindexes);
 	}
 
 
@@ -102,23 +113,90 @@ namespace suboo {
 
 	std::pair<int, BuildOrder> AddMineralGatherer::improve(const BuildOrder & base)
 	{
+		std::vector<BuildOrder> candidates;
+		candidates.reserve(base.getItems().size());
+		std::vector<std::string> candnames;
 		int nexi = base.getFinal().countUnit(UnitId::PROTOSS_NEXUS);
 		int ass = base.getFinal().countUnit(UnitId::PROTOSS_ASSIMILATOR);
 		int prob = base.getFinal().countUnit(UnitId::PROTOSS_PROBE);
+		bool needpylon = false;
+		if (base.getFinal().getAvailableSupply() == 0) {
+			needpylon = true;
+		}
+		auto & tech = TechTree::getTechTree();
+		int available = tech.getInitial().getAvailableSupply();
 		if (base.getFinal().getMinerals() < 20 && (20 * nexi + 3*ass) > prob) {
-			BuildOrder best = base;
-			for (int i = 0, e = best.getItems().size(); i < e; i++) {
-				BuildOrder candidate = best;				
-				candidate.insertItem(UnitId::PROTOSS_PROBE, i);
-				if (timeBO(candidate)) {
-					int delta = best.getFinal().getTimeStamp() - candidate.getFinal().getTimeStamp();
-					if (delta > 0) {
-						return { delta, candidate };
+			for (int i = 0, e = base.getItems().size(); i < e; i++) {
+				if (available >= 1) {
+					BuildOrder candidate = base;
+
+					candidate.insertItem(UnitId::PROTOSS_PROBE, i);
+					if (needpylon) {
+						if (i <= 3) {
+							candidate.insertItem(UnitId::PROTOSS_PYLON, 0);
+						}
+						else {
+							candidate.insertItem(UnitId::PROTOSS_PYLON, i - 3);
+						}
+					}
+					candidates.emplace_back(candidate);
+					candnames.emplace_back("Add Probe at index " + std::to_string(i));
+					// find the next pylons and bring them forward one
+					for (int j = i + 2; j < e; j++) {
+						auto & act = candidate.getItems()[j];
+						if (act.getAction() == BUILD && act.getTarget() == UnitId::PROTOSS_PYLON) {
+							candidate.swapItems(j, j - 1);
+							candidates.emplace_back(candidate);
+							candnames.emplace_back("Add Probe at index " + std::to_string(i) + " and shift pylon at index " + std::to_string(j));							
+						}
+					}
+
+				}
+				auto & bi = base.getItems()[i];
+				available += tech.getUnitById(bi.getTarget()).food_provided;
+			}			
+		}
+		return findBest(base,candidates,candnames);	
+	}
+
+	std::pair<int, BuildOrder> AddProduction::improve(const BuildOrder & base)
+	{
+		auto & tech = TechTree::getTechTree();
+		std::unordered_map<UnitId, int> used;
+		for (auto & bi : base.getItems()) {
+			if (bi.getAction() == BUILD) {
+				auto & u = tech.getUnitById(bi.getTarget());
+				if (u.builder != UnitId::INVALID && !sc2util::IsWorkerType(u.builder)) {
+					auto it = used.find(u.builder);
+					if (it == used.end()) {
+						used[u.builder] = 1;
+					}
+					else {
+						it->second++;
 					}
 				}
 			}
 		}
-		return std::pair<int, BuildOrder>(0, BuildOrder());	
+		std::vector<BuildOrder> candidates;
+		candidates.reserve(base.getItems().size());
+		std::vector<std::string> candnames;
+		for (auto & pair : used) {
+			if (pair.second > base.getFinal().countUnit(pair.first)) {
+				auto & builder = tech.getUnitById(pair.first);
+				// try to stutter
+				int index = 0;
+				for (auto & bi : base.getItems()) {
+					if (bi.getAction() == BUILD && bi.getTarget() == pair.first) {
+						auto candidate = base;
+						candidate.insertItem(pair.first, index);
+						candidates.emplace_back(candidate);
+						candnames.push_back("Add production " + builder.name + " at step " + std::to_string(index));
+					}
+					index++;
+				}
+			}
+		}
+		return findBest(base, candidates, candnames);
 	}
 
 }
