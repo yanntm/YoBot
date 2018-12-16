5d678448c9dfccf0e7ba66ec61cf420328c14023
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-16 01:59:22 +0100

    simu api

--- BuildOrder.cpp
+++ BuildOrder.cpp
@@ -1,4 +1,5 @@
 #include "BuildOrder.h"
+#include <iostream>
 #include <mutex>
 
 namespace suboo {
@@ -11,7 +12,7 @@ namespace suboo {
 		desiredPerUnit[index] += qty;
 	}
 
-	void BuildGoal::print(std::ostream & out)
+	void BuildGoal::print(std::ostream & out) const
 	{
 		auto & tech = TechTree::getTechTree();
 		out << "Goal time=" << framesToCompletion << std::endl;
@@ -52,6 +53,8 @@ namespace suboo {
 			bi.print(out);
 			out << std::endl;
 		}
+		out << "Reached state at " << final.getTimeStamp()  << std::endl;
+		final.print(out);
 	}
 	void BuildOrder::addItem(UnitId tocreate)
 	{		
@@ -61,17 +64,133 @@ namespace suboo {
 	{
 		items.push_front(BuildItem(tocreate));
 	}
-	void BuildItem::print(std::ostream & out)
+	void BuildItem::print(std::ostream & out) const
 	{
 		auto & tech = TechTree::getTechTree();
 		out << "Build " << tech.getUnitById(target).name;
 	}
 	bool GameState::hasUnit(UnitId unit) const
 	{
-		return std::any_of(units.begin(), units.end(), [unit](auto & u) {return u.type == unit; });
+		return std::any_of(units.begin(), units.end(), [unit](auto & u) {return u.type == unit && u.state != u.BUILDING; });
 	}
 	void GameState::addUnit(UnitId unit)
 	{
-		units.emplace_back(UnitInstance(unit));
+		addUnit(UnitInstance(unit));		
+	}
+	void GameState::addUnit(const UnitInstance & unit)
+	{
+		units.emplace_back(unit);
+		vps = -1.0;
+		mps = -1.0;
+	}
+	float GameState::getMineralsPerSecond() const
+	{
+		if (mps == -1.0) {
+			mps = 0;
+			for (auto & u : units) {
+				if (u.type == UnitId::PROTOSS_PROBE) {
+					mps += 0.625; // arbitrary
+				}
+			}
+		}
+		return mps;
+	}
+	float GameState::getVespenePerSecond() const
+	{
+		if (vps == -1.0) {
+			vps = 0;
+			for (auto & u : units) {
+				if (u.type == UnitId::PROTOSS_PROBE && u.state == u.MINING_VESPENE) {
+					vps += 0.625; // arbitrary
+				}
+			}
+		}
+		return vps;
+	}
+	void GameState::stepForward(int secs)
+	{
+		for (int i = 0; i < secs; i++) {
+			mps = -1;
+			vps = -1;
+			for (auto & u : units) {
+				if (u.time_to_free > 0) {
+					u.time_to_free--;
+					if (u.time_to_free == 0) {
+						u.state = u.FREE;
+					}
+				}
+			}
+			minerals += getMineralsPerSecond() ;
+			vespene += getVespenePerSecond() ;
+			timestamp += 1;
+		}
+		
+	}
+	bool GameState::waitForResources(int mins, int vesp)
+	{
+		if (mins > minerals && getMineralsPerSecond() > 0) {
+			int secs = (mins - minerals) / mps;
+			stepForward(secs);
+			std::cout << "Waited for minerals for " << secs << "s." << std::endl;
+		}
+		else {
+			return false;
+		}
+		if (vesp > vespene && getVespenePerSecond() > 0) {
+			stepForward((vesp - vespene) / vps);
+		}
+		else {
+			return false;
+		}
+		return true;
+	}
+	bool GameState::waitforUnitCompletion(UnitId id)
+	{
+		auto it = std::find_if(units.begin(), units.end(), [id](auto & u) {return u.type == id && u.state == u.BUILDING; });
+		if (it == units.end()) {
+			return false;
+		}
+		else {
+			stepForward(it->time_to_free);
+			return true;
+		}
+	}
+	void GameState::print(std::ostream & out) const
+	{
+		auto & tech = TechTree::getTechTree();
+		for (auto & u : units) {
+			u.print(out);
+			out << ",";
+		}
+		out << std::endl;
+		out << "bank : minerals = " << minerals << " vespene = " << vespene << std::endl;
+	}
+	UnitInstance::UnitInstance(UnitId type)
+		: type(type), state(FREE), time_to_free(0) 
+	{
+	}
+	void UnitInstance::print(std::ostream & out) const
+	{
+		out << TechTree::getTechTree().getUnitById(type).name;
+		std::string status;
+		switch (state) {
+		case BUILDING :
+			status = "B";
+			break;
+		case MINING_MINERALS :
+			status = "M";
+			break;
+		case MINING_VESPENE :
+			status = "V";
+			break;
+		case FREE:
+		default:
+			status = "F";
+			break;
+		}
+		out << "(" << status;
+		if (time_to_free > 0)
+			out << time_to_free;
+		out << ")";
 	}
 }
