82db2030b3fae105c7c7b16a798fc5a3d45b4cbb
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-22 14:29:16 +0100

    disabling IO (traces) don't add pylons if food is available through a nexus addProductionForceful bails out at first positive response (sorted by "wait on" time)

--- BOBuilder.cpp
+++ BOBuilder.cpp
@@ -155,14 +155,18 @@ namespace suboo {
 	BuildOrder BOBuilder::improveBO(const BuildOrder & bo, int depth)
 	{
 		std::vector<pboo> optimizers;
+		std::vector<pboo> fastoptimizers;
 		optimizers.emplace_back(new NoWaitShifter());
+		fastoptimizers.emplace_back(new NoWaitShifter());
+		optimizers.emplace_back(new AddMineralGathererStack());
+		optimizers.emplace_back(new AddProductionForceful());
 		optimizers.emplace_back(new AddMineralGatherer());
 		optimizers.emplace_back(new AddProduction());
 		optimizers.emplace_back(new LeftShifter());
+		fastoptimizers.emplace_back(new LeftShifter());
 		optimizers.emplace_back(new AddVespeneGatherer());
-		optimizers.emplace_back(new AddMineralGathererStack());
-		optimizers.emplace_back(new AddProductionForceful());
-
+		
+		
 		if (depth == 0)
 			return bo;
 		//
@@ -174,22 +178,33 @@ namespace suboo {
 			for (auto & p : optimizers) {
 				int optgain = 0;
 				// nested loop mostly does not help, most rules already try many positions it's redundant
-				do {
-					optgain = 0;
-					auto res = p->improve(best,depth);
+				auto res = p->improve(best, depth);
 					if (res.first > 0) {
 						gain += res.first;
 						optgain += res.first;
 						best = res.second;
-						std::cout << "Improved results using " << p->getName() << " by " << res.first << " s. Current best timing :" << best.getFinal().getTimeStamp() << "s at depth " <<  depth <<  std::endl;
+						std::cout << "Improved results using " << p->getName() << " by " << res.first << " delta. Current best timing :" << best.getFinal().getTimeStamp() << "s at depth " << depth << std::endl;
 						//best.print(std::cout);
 					}
 					else {
 						//std::cout << "No improvement of results using " << p->getName() << ". Current best timing :" << best.getFinal().getTimeStamp() << "s" << std::endl;
 					}
-				} while (optgain >0);
+					do {
+						optgain = 0;
+
+						for (auto & p : fastoptimizers) {
+							auto res = p->improve(best, depth);
+							if (res.first > 0) {
+								gain += res.first;
+								optgain += res.first;
+								best = res.second;
+								std::cout << "Improved results (fast) using " << p->getName() << " by " << res.first << " delta. Current best timing :" << best.getFinal().getTimeStamp() << "s at depth " << depth << std::endl;
+								//best.print(std::cout);
+							}
+						}
+					} while (optgain > 0);
 			}
-		} while (gain > 0);		
+		} while (gain > 0);
 
 		return best;
 	}
