c7f8aedf7772d31d5f834f4b7ef37a650da0e469
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2018-12-24 00:04:30 +0100

    more complex run confi structures

--- YoBotMain.cpp
+++ YoBotMain.cpp
@@ -6,9 +6,11 @@
 #include "LadderInterface.h"
 
 //#define _GENTECHTREE 1
-#define _BOSEARCH 1
+//#define _BOSEARCH 1
+#define _SELFPLAY 1
+//#define _RELEASE 1
+//#undef _RELEASE
 
-#ifdef DEBUG
 #ifdef _GENTECHTREE 
 #include "BuildOrder.h"
 #include "TechBot.h"
@@ -61,8 +63,65 @@ int main(int argc, char* argv[])
 		std::cout << "There was a problem loading the map : " << map << std::endl;
 	}
 }
-#else
+#endif
+
+#ifdef _SELFPLAY
+#include "YoBot.h"
+
+class Human : public sc2::Agent {
+
+};
+
+
+int main(int argc, char* argv[])
+{
+	YoBot bot;
+	YoBot bot2;
+	Human h;
+	sc2::Coordinator coordinator;
+	if (!coordinator.LoadSettings(argc, argv))
+	{
+		std::cout << "Unable to find or parse settings." << std::endl;
+		return 1;
+	}
+	coordinator.SetStepSize(1);
+	coordinator.SetRealtime(false);
+	coordinator.SetMultithreaded(true);
+	coordinator.SetParticipants({
+		/*CreateParticipant(sc2::Race::Zerg, &h),*/ CreateParticipant(sc2::Race::Protoss, &bot)//,CreateParticipant(sc2::Race::Protoss, &bot2)
+		//sc2::PlayerSetup(sc2::PlayerType::Observer,Util::GetRaceFromString(enemyRaceString)),
+		,CreateComputer(sc2::Race::Random, sc2::Difficulty::VeryHard)
+		
+	});
+	coordinator.LaunchStarcraft();	// Start the game.
+
+	//auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\AcidPlantLE.SC2Map"; // "AcidPlant LE";  //"Interloper LE""16-Bit LE"
+	// auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\Redshift.SC2Map";
+	// auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\DarknessSanctuary.SC2Map";
+	//auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\16BitLE.SC2Map";
+	//auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\InterloperLE.SC2Map";
+	//auto map = "D:\\games\\StarCraft II\\Maps\\LostAndFoundLE.SC2Map";
+	// auto map = "D:\\games\\StarCraft II\\Maps\\ParaSiteLE.SC2Map";
+	//auto map = "D:\\games\\StarCraft II\\Maps\\AutomatonLE.SC2Map";
+	auto map = "D:\\games\\StarCraft II\\Maps\\CeruleanFallLE.SC2Map";
+	//coordinator.StartGame("C:/Program Files (x86)/StarCraft II/Maps/InterloperLE.SC2Map");
+	//coordinator.StartGame();
+	// coordinator.StartGame("16-Bit LE");
+	if (coordinator.StartGame(map)) {
+		// Step forward the game simulation.
+		while (coordinator.Update())
+		{
+		}
+	}
+	else {
+		std::cout << "There was a problem loading the map : " << map << std::endl;
+	}
+}
+#endif
+
+
 #ifdef _BOSEARCH
+
 #include "BuildOrder.h"
 #include "BOBuilder.h"
 
@@ -72,7 +131,7 @@ using namespace suboo;
 int main(int argc, char* argv[])
 {
 	BOBuilder builder;
-	   
+
 	BuildGoal goal(0); // ASAP
 	goal.addUnit(UnitId::PROTOSS_IMMORTAL, 1);
 	//goal.addUnit(UnitId::PROTOSS_DISRUPTOR, 1);
@@ -81,20 +140,23 @@ int main(int argc, char* argv[])
 	//goal.addUnit(UnitId::PROTOSS_STARGATE, 3);
 	goal.addUnit(UnitId::PROTOSS_OBSERVER, 1);
 	goal.addUnit(UnitId::PROTOSS_PHOENIX, 3);
-	goal.addUnit(UnitId::PROTOSS_ZEALOT, 15);
-	
+	goal.addUnit(UnitId::PROTOSS_ZEALOT, 10); //goal.addUnit(UnitId::PROTOSS_NEXUS, 1);
+
 	builder.addGoal(goal);
 	goal.print(std::cout);
 
 	BuildOrder bo = builder.computeBO();
 	std::cout << "Initial realizable :" << std::endl;
 	bo.print(std::cout);
-	auto boopt = builder.improveBO(bo);
-	
+	auto boopt = builder.improveBO(bo, 2);
+
 	std::cout << "Final realizable :" << std::endl;
 	boopt.print(std::cout);
-	timeBO(boopt);
-	{
+	auto boopt2 = builder.improveBO(boopt, 5);
+	std::cout << "Final realizable :" << std::endl;
+	boopt2.print(std::cout);
+
+	if (true) {
 		BuildOrder bo;
 		bo.addItem(UnitId::PROTOSS_PROBE);
 		bo.addItem(UnitId::PROTOSS_PYLON);
@@ -107,7 +169,7 @@ int main(int argc, char* argv[])
 		bo.addItem(UnitId::PROTOSS_ZEALOT);
 		bo.addItem(UnitId::PROTOSS_PROBE);
 		bo.addItem(UnitId::PROTOSS_ASSIMILATOR);
-		bo.addItem(TRANSFER_VESPENE); 
+		bo.addItem(TRANSFER_VESPENE);
 		bo.addItem(UnitId::PROTOSS_PYLON);
 		bo.addItem(UnitId::PROTOSS_PROBE);
 		bo.addItem(UnitId::PROTOSS_ZEALOT);
@@ -152,7 +214,7 @@ int main(int argc, char* argv[])
 		std::cout << "build by voxel :" << std::endl;
 		bo.print(std::cout);
 
-		auto boopt2 = builder.improveBO(bo);
+		auto boopt2 = builder.improveBO(bo, 3);
 
 		std::cout << "Final realizable :" << std::endl;
 		boopt2.print(std::cout);
@@ -164,62 +226,10 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-#else
 
-#include "YoBot.h"
-
-class Human : public sc2::Agent {
-
-};
-
-
-int main(int argc, char* argv[])
-{
-	YoBot bot;
-	YoBot bot2;
-	Human h;
-	sc2::Coordinator coordinator;
-	if (!coordinator.LoadSettings(argc, argv))
-	{
-		std::cout << "Unable to find or parse settings." << std::endl;
-		return 1;
-	}
-	coordinator.SetStepSize(1);
-	coordinator.SetRealtime(false);
-	coordinator.SetMultithreaded(true);
-	coordinator.SetParticipants({
-		/*CreateParticipant(sc2::Race::Zerg, &h),*/ CreateParticipant(sc2::Race::Protoss, &bot)//,CreateParticipant(sc2::Race::Protoss, &bot2)
-		//sc2::PlayerSetup(sc2::PlayerType::Observer,Util::GetRaceFromString(enemyRaceString)),
-		,CreateComputer(sc2::Race::Random, sc2::Difficulty::VeryHard)
-		
-	});
-	// Start the game.
-	coordinator.LaunchStarcraft();
-	//auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\AcidPlantLE.SC2Map"; // "AcidPlant LE";  //"Interloper LE""16-Bit LE"
-	// auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\Redshift.SC2Map";
-	// auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\DarknessSanctuary.SC2Map";
-	//auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\16BitLE.SC2Map";
-	//auto map = "G:\\Program Files (x86)\\StarCraft II\\Maps\\InterloperLE.SC2Map";
-	//auto map = "D:\\games\\StarCraft II\\Maps\\LostAndFoundLE.SC2Map";
-	// auto map = "D:\\games\\StarCraft II\\Maps\\ParaSiteLE.SC2Map";
-	//auto map = "D:\\games\\StarCraft II\\Maps\\AutomatonLE.SC2Map";
-	auto map = "D:\\games\\StarCraft II\\Maps\\CeruleanFallLE.SC2Map";
-	//coordinator.StartGame("C:/Program Files (x86)/StarCraft II/Maps/InterloperLE.SC2Map");
-	//coordinator.StartGame();
-	// coordinator.StartGame("16-Bit LE");
-	if (coordinator.StartGame(map)) {
-		// Step forward the game simulation.
-		while (coordinator.Update())
-		{
-		}
-	}
-	else {
-		std::cout << "There was a problem loading the map : " << map << std::endl;
-	}
-}
 #endif
-#endif
-#else 
+
+#ifdef _RELEASE
 
 #include "YoBot.h"
 
